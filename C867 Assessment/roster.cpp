70cea5c1175cf41975772c2aa7cae0cd50f057ea
Scott Morales <morales.scott@gmail.com>
2019-04-18 15:54:38 -0500

    Clean up extra code. Everything works - last thing is add / remove student.

--- C867 Assessment/roster.cpp
+++ C867 Assessment/roster.cpp
@@ -1,8 +1,7 @@
 #include "roster.h"
 
 Roster::Roster(int maxSize) {
-	//Provided Data Table
-	const string studentData[] = {
+	const string studentData[] = {  //Provided Data Table
 		"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY"		,
 		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK"	,
 		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE"		,
@@ -112,32 +111,22 @@ void Roster::printByDegreeProgram(Degree degreeProgram) {
 int main() {
 	int maxSize = 5;
 	
-	//Print out to the screen, via your application, the course title, the programming language used, your student ID, and your name.
 	cout << "C867 Scripting and Programming\t C++    #000954923   Robert Morales" << endl << endl;
 	
 	Roster* classRoster = nullptr; 
 	classRoster = new Roster(maxSize);   //create classRoster, add each student to classRoster
 
-//	classRoster->printAll();
+	classRoster->printAll();
 
-//	classRoster->printInvalidEmails();
+	classRoster->printInvalidEmails();
 
-	//classRoster->printAverageDaysInCourse("A4"); //This works to call single student's avgDays
-//	for (int i = 0; i < maxSize; ++i) { //loop through all students and print avgDays
-//		ostringstream idOSS;
-//		idOSS << "A" << i+1;
-//		classRoster->printAverageDaysInCourse(idOSS.str()); 
-//	}
+	for (int i = 0; i < maxSize; ++i) { //loop through printAverageDaysInCourse("A1")
+		ostringstream idOSS;
+		idOSS << "A" << i+1;
+		classRoster->printAverageDaysInCourse(idOSS.str()); 
+	}
 
 	classRoster->printByDegreeProgram(SOFTWARE);
-	
-	
-	//***How to send arr as argument and return arr as pointer ***
-	/*int days[3] = {10, 20, 30};
-	Student poopMan = Student("a1", "bob", "smith", "yahoo@gmail.com", "36", days, SOFTWARE);
-	int *tempdays; tempdays = poopMan.GetDaysInCourse();
-	cout << poopMan.GetFirstName() << endl;
-	for(int i = 0; i < 3; ++i) cout << tempdays[i] << endl;*/
 	
 	char exitSign = 'a';
 	while (exitSign != 'q') {
